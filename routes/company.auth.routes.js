0646c1e99f062c6f96d3977738d452cc426339bf
Hugo Lomba <hmlomba@gmail.com>
2022-11-19 19:15:04 -0300

    add rota verify in compnay

--- routes/company.auth.routes.js
+++ routes/company.auth.routes.js
@@ -2,6 +2,7 @@ const bcrypt = require("bcryptjs");
 const saltRounds = 10;
 const jwt = require("jsonwebtoken");
 const router = require("express").Router();
+const { isAuthenticated } = require("../middlewares/jwt.middleware");
 
 const Company = require("../models/Company.model");
 
@@ -141,6 +142,16 @@ router.post("/login", (req, res, next) => {
       res.status(500).json({ message: "Internal Server Error", error: err });
       console.log(err);
     });
+});
+
+// ROTA DE VERIFICAÇÃO
+
+router.get("/verify", isAuthenticated, (req, res, next) => {
+  try {
+    res.status(200).json({ authenticatedUser: req.payload });
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
